8bfef94bc8b4e85bbe61ba8625517dd5ddd89da0
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-09-07 13:34:58 -0500

    Add last commit homologation, tests for examples

--- tests/unit/core/index.spec.js
+++ tests/unit/core/index.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import jsf from '../../../src/lib';
 
-describe('jsf', () => {
+describe('jsf generate', () => {
   it('Take example without validation', () => {
     const schema = {
       type: 'integer',
@@ -20,12 +20,38 @@ describe('jsf', () => {
     jsf.option('useExamplesValue', true);
     expect(jsf.generate(schema, undefined, () => { return []; })).to.eql(123);
   });
-
   it('Generate fake data when example is not valid', () => {
     const schema = {
       type: 'integer',
       format: 'int32',
       example: '123',
+    };
+    jsf.option('useExamplesValue', true);
+    expect(jsf.generate(schema, undefined, () => { return [{ error: 'some' }]; })).to.be.an('number');
+  });
+  it('Take one example from examples without validation', () => {
+    const schema = {
+      type: 'integer',
+      format: 'int32',
+      examples: [123, 456],
+    };
+    jsf.option('useExamplesValue', true);
+    expect([123, 456]).to.include(jsf.generate(schema));
+  });
+  it('Take one example from examples with validation', () => {
+    const schema = {
+      type: 'integer',
+      format: 'int32',
+      examples: [123, 456],
+    };
+    jsf.option('useExamplesValue', true);
+    expect([123, 456]).to.include(jsf.generate(schema, undefined, () => { return []; }));
+  });
+  it('Generate fake data when example from examples is not valid', () => {
+    const schema = {
+      type: 'integer',
+      format: 'int32',
+      examples: ['123', '456'],
     };
     jsf.option('useExamplesValue', true);
     expect(jsf.generate(schema, undefined, () => { return [{ error: 'some' }]; })).to.be.an('number');
