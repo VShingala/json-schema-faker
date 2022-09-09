d7c08856e1aee4131dc4640071ffa8406497e175
Carlos_Veloz <carlos.veloz@wizeline.com>
2022-09-09 12:02:59 -0500

    fixing lint issues

--- tests/integration/umdwrapper.test.js
+++ tests/integration/umdwrapper.test.js
@@ -5,7 +5,7 @@ describe('UMD Wrapper', () => {
     const jsf = require('../../src/main.iife').default;
 
     expect(jsf).not.to.be.undefined;
-    //expect(jsf.VERSION).not.to.be.undefined;
+    // expect(jsf.VERSION).not.to.be.undefined;
     expect(jsf.generate).not.to.be.undefined;
     expect(jsf.generate({ const: 42 })).to.eql(42);
   });
